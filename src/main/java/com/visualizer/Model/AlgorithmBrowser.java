c4ba3263f84ac0a5e41a7151b218765b995e2bbe
yewspine <yewspine.game@gmail.com>
2025-05-21 14:15:55 +0200

    feat(AlgorithmBrowser) Create interface for The AlgorithmBrowser Controller

---
+++ src/main/java/com/visualizer/Model/AlgorithmBrowser.java
@@ -0,0 +1,10 @@
+package com.visualizer.Model;
+
+import javafx.event.ActionEvent;
+
+import com.visualizer.DomainObject.Algorithm;
+
+public interface AlgorithmBrowser
+{
+  public toggleCategory(ActionEvent event);
+}
