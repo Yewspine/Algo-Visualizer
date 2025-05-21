87f69f01e43c3d26f7ca1d642df4c2ff8cd6203c
yewspine <yewspine.game@gmail.com>
2025-05-21 14:07:27 +0200

    feat(Algorithm.java) Create a domain object to contain every avaialable Algorithm

---
+++ src/main/java/com/visualizer/DomainObject/Algorithm.java
@@ -0,0 +1,30 @@
+package com.visualizer.DomainObject;
+
+public static class Algorithm
+{
+  private final String algorithm_type;
+  private final String algorithm_name;
+  
+  public Algorithm(String algorithm_type, String algorithm_name)
+  {
+    this.algorithm_type = algorithm_type;
+    this.algorithm_name = algorithm_name;
+  }
+  
+  public String getAlgorithmType()
+  {
+    return algorithm_type;
+  }
+
+  public String getAlgorithmName()
+  {
+    return algorithm_name;
+  }
+
+  @Override
+  public String toString()
+  {
+    return algorithm_type + " : ( " + algorithm_type + " ) ";
+  }
+
+}
