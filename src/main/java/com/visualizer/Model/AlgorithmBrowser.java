da16b31064f07cc7d46c882dde30e0964977f584
yewspine <yewspine.game@gmail.com>
2025-05-24 08:12:31 +0200

    feat(View, ViewModel): Patch The view and the controller of AlgorithmBrowser to support the reflection

--- src/main/java/com/visualizer/Model/AlgorithmBrowser.java
+++ src/main/java/com/visualizer/Model/AlgorithmBrowser.java
@@ -1,10 +1,16 @@
 package com.visualizer.Model;
 
+import java.util.List;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 
-import com.visualizer.DomainObject.Algorithm;
+import com.visualizer.Model.Algorithm;
 
 public interface AlgorithmBrowser
 {
+  List<Algorithm> getDiscoverableAlgorithms();
+  ObjectProperty<ObservableList<Algorithm>> filteredAlgorithmsProperty();
   void toggleCategory(ActionEvent event);
 }
