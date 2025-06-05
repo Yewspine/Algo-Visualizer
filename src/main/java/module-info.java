891b14c5984a44739e3e005cf1969778eb31b307
yewspine <yewspine.game@gmail.com>
2025-06-05 19:05:33 +0200

    fix: Exports more module for javadoc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,11 @@
 module com.visualizer {
-    requires javafx.controls;
-    requires org.reflections;
-    exports com.visualizer;
+  requires javafx.controls;
+  requires org.reflections; 
+  exports com.visualizer.Converter;
+  exports com.visualizer.DomainObject;
+  exports com.visualizer.DomainObject.Algorithms;
+  exports com.visualizer.Model;
+  exports com.visualizer.View;
+  exports com.visualizer.View.Fragment;
+  exports com.visualizer.ViewModel;
 }
