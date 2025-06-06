02b1cfd4c447e4dfa7f7a0f0909ac7eaf77bf929
yewspine <yewspine.game@gmail.com>
2025-06-06 08:51:48 +0200

    doc: Add comments

--- src/main/java/com/visualizer/App.java
+++ src/main/java/com/visualizer/App.java
@@ -10,18 +10,23 @@ import com.visualizer.View.MainView;
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class App extends Application 
+{
+  /**
+   * Constructor the main view and the main scene then render it
+   * @param stage the stage to draw the scene on
+   * @throws Exception Internal Java Exception, since various Exception can be throw I goes with a generic one
+   * */
+  @Override
+  public void start(Stage stage) throws Exception { 
+    MainView view = new MainView();
+    Scene scene = new Scene(view, 800, 600); 
+    stage.setScene(scene);
+    stage.show();
+  }
 
-    @Override
-    public void start(Stage stage) throws Exception { 
-      MainView view = new MainView();
-      Scene scene = new Scene(view, 800, 600); 
-      stage.setScene(scene);
-      stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
+  public static void main(String[] args) {
+    launch();
+  }
 
 }
