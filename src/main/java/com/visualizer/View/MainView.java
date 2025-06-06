4d54b4e7164a5e90e8e6c98844346ad47c3f2b98
yewspine <yewspine.game@gmail.com>
2025-06-06 09:01:26 +0200

    feat: Implement Event_broker and center View

--- src/main/java/com/visualizer/View/MainView.java
+++ src/main/java/com/visualizer/View/MainView.java
@@ -2,10 +2,11 @@ package com.visualizer.View;
 
 import javafx.scene.layout.BorderPane;
 
-import com.visualizer.ViewModel.AlgorithmBrowserViewModel;
-import com.visualizer.ViewModel.SelectorFragmentViewModel;
+import com.visualizer.DomainObject.Coordinator;
+import com.visualizer.View.Composition.CenterViewComposition;
 import com.visualizer.View.Fragment.AlgorithmBrowserFragment;
 import com.visualizer.View.Fragment.SelectorFragment;
+import com.visualizer.ViewModel.AlgorithmBrowserViewModel;
 
 /**
  * The main view class It is a BorderPane to easily split the different parts
@@ -16,9 +17,12 @@ public class MainView extends BorderPane
 {
   public MainView()
   {
-    AlgorithmBrowserFragment algorithm_browser_fragment = new AlgorithmBrowserFragment(new AlgorithmBrowserViewModel()); 
-    SelectorFragment selector_fragment = new SelectorFragment(new SelectorFragmentViewModel());
+    Coordinator event_broker = new Coordinator();
+    AlgorithmBrowserFragment algorithm_browser_fragment = new AlgorithmBrowserFragment(new AlgorithmBrowserViewModel(), event_broker); 
+    SelectorFragment selector_fragment = new SelectorFragment(event_broker);
+    CenterViewComposition center_view = new CenterViewComposition(event_broker);
     this.setTop(selector_fragment);
     this.setLeft(algorithm_browser_fragment);
+    this.setCenter(center_view);
   }
 }
