3bea6ec0196cff93f880ea4c88ba504704120e39
yewspine <yewspine.game@gmail.com>
2025-06-19 02:42:38 +0200

    fix: Change the module-info to reflect the new used modules and remove some debug stuff that do not exist anymore in SytemInfo as well as use the current Selector Instead of MainView

--- src/main/java/com/visualizer/Converter/PairStringConverter.java
+++ src/main/java/com/visualizer/Converter/PairStringConverter.java
@@ -1,6 +1,7 @@
 package com.visualizer.Converter;
 
 import java.lang.UnsupportedOperationException;
+
 import javafx.util.Pair;
 import javafx.util.StringConverter;
 
@@ -8,12 +9,12 @@ import javafx.util.StringConverter;
  * This class is used to convert a Pair of String to a common String.
  * @author Yewspine
  * */
-public class PairStringConverter extends StringConverter<Pair<String, String>>
+public class PairStringConverter extends StringConverter<Pair<String, String>> 
 {
-  
   public PairStringConverter()
   {}
 
+  
   /**
    * The overriden <pre>toString()</pre> method extract the key of the pair and return it
    * In case the pair or the key is null, an empty String is sent
@@ -25,10 +26,11 @@ public class PairStringConverter extends StringConverter<Pair<String, String>>
   @Override
   public String toString(Pair<String, String> pair)
   {
-    return pair != null && pair.getKey() != null ? pair.getKey() : ""; 
+    boolean pairContainValue = ( pair != null && pair.getKey() != null ); 
+    return pairContainValue ? pair.getKey() : "";
   }
 
-  /**
+    /**
    * Normally it should create a <pre>{@code Pair<String, String>}</pre> from a 
    * <pre>String</pre> However in this context, this is useless and wil always throw an Exception
    * @param string the <pre>String</pre> value to convert to a Pair of String 
@@ -38,6 +40,6 @@ public class PairStringConverter extends StringConverter<Pair<String, String>>
   @Override
   public Pair<String, String> fromString(String string)
   {
-    throw new UnsupportedOperationException("Operation not supported in this context");
+    throw new UnsupportedOperationException();
   }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,20 +1,24 @@
 module com.visualizer {
   requires javafx.controls;
+  /*
   requires org.fxmisc.richtext;
   requires org.fxmisc.flowless;
+  */
   exports com.visualizer;
   exports com.visualizer.Converter;
+  /*
   exports com.visualizer.DomainObject;
   exports com.visualizer.DomainObject.Algorithms;
+  */
   exports com.visualizer.Model;
-  exports com.visualizer.View;
-  exports com.visualizer.View.Fragment;
-  exports com.visualizer.ViewModel;
+  exports com.visualizer.View.Selector;
+  exports com.visualizer.ViewModel.Selector;
 
+  /*
   uses com.visualizer.Model.Algorithm;
   provides com.visualizer.Model.Algorithm with
     com.visualizer.DomainObject.Algorithms.Sort.BubbleSort,
     com.visualizer.DomainObject.Algorithms.PathFinding.Djikstra;
-
+*/
 
 }
